ee8ba5fad8917ba36bf36fb0b7e7decf70ccd5f0
codigoenlaweb <codeenlaweb@gmail.com>
2022-05-25 14:57:00 -0400

    param urls

--- src/components/search/SearchScreen.js
+++ src/components/search/SearchScreen.js
@@ -1,7 +1,36 @@
-import React from 'react'
+import React from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useForm } from "../../hooks/UseForm";
 
 export const SearchScreen = () => {
+  const {onInputChange, formState, onResetForm} = useForm({search_hero:""})
+  const navigate = useNavigate()
+  const location = useLocation()
+  console.log(location);
+
+  const handleSearch = (e) => {
+    e.preventDefault()
+    console.log(formState);
+    navigate(`?q=${formState.search_hero}&pepe=fsfs`)
+    onResetForm()
+  }
+
+  
+
   return (
-    <div>SearchScreen</div>
-  )
-}
+    <main className="py-2 px-2 md:py-4 md:px-4">
+      <form onSubmit={handleSearch} className="flex justify-center items-center">
+        <input
+          className="w-56 md:w-72 px-1 md:px-2 bg-gray-300 rounded-sm h-7 font-bold placeholder:font-bold text-gray-600 placeholder:text-gray-400"
+          type="text"
+          name="search_hero"
+          autoComplete="off"
+          placeholder="Search hero..."
+          value={formState.name}
+          onChange={onInputChange}
+        />
+        <button className="ml-2 font-bold text-pink-500 py-1 px-2 border border-solid border-pink-500 rounded-md outline-white hover:bg-pink-500 focus:bg-pink-500 hover:text-white focus:text-white transition duration-300">Search</button>
+      </form>
+    </main>
+  );
+};
--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -6,10 +6,10 @@ export const Navbar = () => {
   return (
     <header className="px-2 py-2 md:px-4 md:py-3 bg-neutral-800 flex flex-wrap justify-between items-center">
       <nav className=" w-full md:w-auto md:text-lg flex flex-row flex-wrap justify-between md:justify-start items-center">
-        <Link to="/" className="text-white font-bold text-2xl md:text-3xl">
+        <Link to="/" className="text-white font-bold text-xl md:text-3xl">
           Heroes App
         </Link>
-        <ul className="md:ml-6 mt-1 flex justify-between w-36 md:w-24">
+        <ul className="md:ml-6 mt-1 flex justify-between w-44 md:w-40 text-sm md:text-base">
           <li className="text-gray-400 font-bold">
             <NavLink
               to="/marvel"
@@ -28,6 +28,16 @@ export const Navbar = () => {
               }
             >
               DC
+            </NavLink>
+          </li>
+          <li className="text-gray-400 font-bold">
+            <NavLink
+              to="/search"
+              className={({ isActive }) =>
+                isActive ? "text-pink-500" : "hover:text-white focus:text-white"
+              }
+            >
+              Search
             </NavLink>
           </li>
           <li className="md:hidden">
